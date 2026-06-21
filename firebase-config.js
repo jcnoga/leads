7c43f313b94ca1157f9bc845fecaf7bfb1ef25cf
jcnoga <164394666+jcnoga@users.noreply.github.com>
2026-06-21 13:18:00 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -14,14 +14,15 @@
  * -----------------------------------------------------------------------
  */
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID",
+  apiKey: "AIzaSyDIQdzfnMBQ9Q6docuSPPbVyJ8PLoKD1AQ",
+  authDomain: "leads-e5ae1.firebaseapp.com",
+  databaseURL: "https://leads-e5ae1-default-rtdb.firebaseio.com",
+  projectId: "leads-e5ae1",
+  storageBucket: "leads-e5ae1.firebasestorage.app",
+  messagingSenderId: "17213040146",
+  appId: "1:17213040146:web:d064ccc567e0b4dfd31acb",
+  measurementId: "G-QSGNSDGJML"
 };
-
 firebase.initializeApp(firebaseConfig);
 
 // URL do Cloudflare Worker que faz a busca de leads sem expor a chave da
